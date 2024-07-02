35c8197b30e0aafbbc73f7ba232a768aa924c06d
eunyoung56 <eunyeong1328@daum.net>
2024-07-02 23:28:51 +0900

    declare and initialize variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        int a; //변수 선언
+        a=10;  //변수 초기화
+
+        System.out.println(a);
     }
 }
