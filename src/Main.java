a296940f0846ca0337cd27689bbfccbbc535374c
eunyoung56 <eunyeong1328@daum.net>
2024-07-02 23:33:18 +0900

    declare and initialize mutiple variables at once

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
         int a; //변수 선언
         a=10;  //변수 초기화
+        int b=3, c=4; //여러 변수 선언과 초기화를 한번에
 
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
